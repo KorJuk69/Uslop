da302cd4dfb5bebde8d18bae18107cf0cd7b54f6
KorJuk69 <korjuk1@gmail.com>
2017-09-11 12:44:16 +0300

    New

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = 	new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
+        if((a==c)||(b==d)){
+            System.out.print("Yes");
+        }
+        else {
+            System.out.print("NO");
+        }
+    }
+}
