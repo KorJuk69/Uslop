59852a2879cc3c5e4342c9d1c322b8a9e1068084
KorJuk69 <korjuk1@gmail.com>
2017-09-11 17:19:30 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
         int k = scanner.nextInt();
+
         int a = k+4;
         int b = a/4;
         int c = b*4-4;
