16b1cccf7f168f4989c86aa8449b93ae26d2a2ad
KorJuk69 <korjuk1@gmail.com>
2017-09-11 17:56:11 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,30 @@ public class Main {
         Scanner scanner = 	new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        int d = scanner.nextInt();
 
         if((a==0)&&(b==0)){
             System.out.print("INF");
         }
         else{
+            if((b%a==0)&&(d%c==0)){
+                //если и числитель и знаменатель = 0
+            }
+            else {
             if (b%a==0){
                 int x = -b/a;
                 System.out.print(x);
             }
             else{
-                System.out.print("NO");
-            }
+                if(d%c==0){
+                    int x = -d/c;
+                    System.out.print(x);
+                }
+                else {
+                    System.out.print("NO");
+                }
+            }}
         }
 
 
