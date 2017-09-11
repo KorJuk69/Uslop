8222a5ad1a591f2cfba807737c7c06b7471c0a8b
KorJuk69 <korjuk1@gmail.com>
2017-09-11 17:04:23 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,12 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int k = scanner.nextInt();
 
-        if (((a==c+1)&&((b==d+2)||(b==d-2)))||((a==c+2)&&((b==d+1)||(b==d-1)))||((a==c-1)&&((b==d+2)||(b==d-2)))||((a==c-2)&&((b==d+1)||(b==d-1)))){
+        if (((k%n==0)||(k%m==0))&&(k!=n)&&(k!=m)){
              System.out.print("Yes");
         }
         else{
