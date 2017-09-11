032aa363f642addfea6b6a3d7d9ef3578da484dd
KorJuk69 <korjuk1@gmail.com>
2017-09-11 17:17:44 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
         int k = scanner.nextInt();
+        int a = k+4;
+        int b = a/4;
+        int c = b*4-4;
 
-        if (((k%n==0)||(k%m==0))&&(k!=n*m)){
+        if (k==c){
              System.out.print("Yes");
         }
         else{
