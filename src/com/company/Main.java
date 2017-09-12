78db4c3611677ae08f550bb0000e331e5216314d
KorJuk69 <korjuk1@gmail.com>
2017-09-12 19:16:29 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,43 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        if((a==c)||(b==d)){
-            System.out.print("Yes");
+        int n = scanner.nextInt();
+        int bo=0;
+        int io=0;
+        int i=0;
+
+        while (n>=60){
+            bo++;
+            n=n-60;
+        }
+        if(n<10){
+            io=1;
         }
         else {
-            System.out.print("NO");
+        while (n>=10){
+            io++;
+            n=n-10;
         }
+        while (n>=1){
+            i++;
+            n--;
+        }}
+
+        int cenabo = 440;
+        int cenaio = 125;
+        int cenai = 15;
+
+        if(cenai*i>cenaio*io){
+            io++;
+            i=0;
+        }
+        if((cenai*i+cenaio*io)>cenabo*bo){
+            bo++;
+            i=0;
+            io=0;
+        }
+
+        System.out.print(i + " " + io + " " + bo);
+
     }
 }
