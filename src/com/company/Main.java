d02dd390fb1ad06d4f8d2226d462a938cd10d4d8
KorJuk69 <korjuk1@gmail.com>
2017-09-12 11:46:51 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,16 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
+        int k = scanner.nextInt();
 
-        a = a * 100;
-        c = c * 100;
-        int e = ((c+d)-(a+b))/100;
-        int f = (c+d)-(a+b) - e*100;
-        System.out.print(e+" "+f);
+        while (k%3!=0){
+            k=k-5;
+        }
+        if(k<0){
+            System.out.print("NO");
+        } else {
+            System.out.print("Yes");
+        }
 
     }
 }
