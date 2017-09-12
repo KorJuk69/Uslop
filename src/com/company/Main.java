aad0ddb9e8dce894a4d8a303c2bc4f233a2aedc5
KorJuk69 <korjuk1@gmail.com>
2017-09-12 12:44:50 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,44 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int x1 = scanner.nextInt();
-        int y1 = scanner.nextInt();
-        int x2 = scanner.nextInt();
-        int y2 = scanner.nextInt();
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
-        if ((x1>0)&&(x2>0)){
-            if(((y1>0)&&(y2>0))||(y1<0)&&(y2<0)){
-                System.out.print("Yes");
-            }
-        } else {
-            if ((x1<0)&&(x2<0)){
-                if(((y1>0)&&(y2>0))||(y1<0)&&(y2<0)){
+        if(a>b){
+            if(a>c){
+                if(a<(b+c)){
                     System.out.print("Yes");
+                }
+                else {
+                    System.out.print("NO");
                 }
             }
             else {
-                System.out.print("NO");
+                if(c<(a+b)){
+                    System.out.print("Yes");
+                }
+                else {
+                    System.out.print("NO");
+                }
+            }
+        }
+        else {
+            if(b>c){
+                if(b<(a+c)){
+                    System.out.print("Yes");
+                }
+                else {
+                    System.out.print("NO");
+                }
+            }
+            else {
+                if(c<(a+b)){
+                    System.out.print("Yes");
+                }
+                else {
+                    System.out.print("NO");
+                }
             }
         }
 
