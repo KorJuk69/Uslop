3429a27a1e64087c837d046749af45df3a851c5d
KorJuk69 <korjuk1@gmail.com>
2017-09-13 11:23:41 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,55 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        if((a==c)||(b==d)){
-            System.out.print("Yes");
+        int x = scanner.nextInt();
+
+        int des = x/10;
+        int od = x - des*10;
+
+        switch (des){
+            case 1:System.out.print("X");
+                break;
+            case 2:System.out.print("XX");
+                break;
+            case 3:System.out.print("XXX");
+                break;
+            case 4:System.out.print("XL");
+                break;
+            case 5:System.out.print("L");
+                break;
+            case 6:System.out.print("LX");
+                break;
+            case 7:System.out.print("LXX");
+                break;
+            case 8:System.out.print("LXXX");
+                break;
+            case 9:System.out.print("XC");
+                break;
+            case 10:System.out.print("C");
+                break;
         }
-        else {
-            System.out.print("NO");
+        switch (od){
+            case 0:System.out.print("");
+                break;
+            case 1:System.out.print("I");
+                break;
+            case 2:System.out.print("II");
+                break;
+            case 3:System.out.print("III");
+                break;
+            case 4:System.out.print("IV");
+                break;
+            case 5:System.out.print("V");
+                break;
+            case 6:System.out.print("VI");
+                break;
+            case 7:System.out.print("VII");
+                break;
+            case 8:System.out.print("VIII");
+                break;
+            case 9:System.out.print("IX");
+                break;
         }
+
     }
 }
