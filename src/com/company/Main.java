8fe063c6d4cfec0745fbddebd596f5fa2a40670e
KorJuk69 <korjuk1@gmail.com>
2017-09-13 10:57:10 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,12 +9,13 @@ public class Main {
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        if((a==c)||(b==d)){
+
+        if( ((a%2==1)||(b%2==1)||(c%2==1))&&((a%2==0)||(b%2==0)||(c%2==0)) ){
             System.out.print("Yes");
         }
         else {
             System.out.print("NO");
         }
+
     }
 }
