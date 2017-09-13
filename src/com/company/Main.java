6fadfa5216cc4689710e6f98e60435e164b08ea0
KorJuk69 <korjuk1@gmail.com>
2017-09-13 09:52:30 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,30 +17,19 @@ public class Main {
             p60++;
             n=n-60;
         }
-        if(n<20){
-            p20=1;
+        while (n>=20){
+            p20++;
+            n=n-20;
         }
-        else {
-            while (n>=20){
-                p20++;
-                n=n-20;
-            }
-            if(n<10){
-                p10=1;
-            }
-            else{
-                while (n>=10){
-                    p10++;
-                    n=n-10;
-                }
-                if(n<5){
-                    p5=1;
-                }
-            }
-            while (n>=1){
-                p1++;
-                n--;
-            }}
+        while (n>=10){
+            p10++;
+            n=n-10;
+        }
+        while (n>=5){
+            p5++;
+            n=n-5;
+        }
+        p1=n;
 
         int cena60 = 440;
         int cena20 = 230;
@@ -73,6 +62,5 @@ public class Main {
 
         System.out.print(p1 + " " + p5 + " " + p10 + " " + p20 + " " + p60);
 
-        // ПЕРЕТЕСТИТЬ И ПЕРЕПИСАТЬ
     }
 }
