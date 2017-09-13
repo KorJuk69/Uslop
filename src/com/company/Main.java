4bede371ec6921d5079a8da727c153f6b3ab5309
KorJuk69 <korjuk1@gmail.com>
2017-09-13 10:33:21 +0300

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,36 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = 	new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
-        int d = scanner.nextInt();
-        if((a==c)||(b==d)){
-            System.out.print("Yes");
+        //Тут немного не такой ввод как в задании, я не стал с этим заморачиваться
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        if((m==x)&&(n!=y)){
+            System.out.println(x + " " + (y+1));
+            System.out.println((x-1) + " " + y);
+            System.out.println(x + " " + (y-1));
         }
-        else {
-            System.out.print("NO");
+        else{
+            if((m!=x)&&(n==y)){
+                System.out.println((x+1) + " " + y);
+                System.out.println((x-1) + " " + y);
+                System.out.println(x + " " + (y-1));
+            }
+            else {
+                if((m==x)&&(n==y)){
+                    System.out.println((x-1) + " " + y);
+                    System.out.println(x + " " + (y-1));
+                }
+                else {
+                    System.out.println((x+1) + " " + y);
+                    System.out.println(x + " " + (y+1));
+                    System.out.println((x-1) + " " + y);
+                    System.out.println(x + " " + (y-1));
+                }
+            }
         }
+
     }
 }
